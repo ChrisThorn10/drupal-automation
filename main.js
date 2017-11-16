5a0f680bd4957b2aa512a6266a503bf7c0df39b5
Christie <thornton.christie@gmail.com>
2017-11-16 04:07:06 -0500

    Script now removes style tags, makes internal links relative, and opens links outside of uncc.edu in a separate window.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+/*---------------------------------------------------------------------------*/
 // package imports
+/*---------------------------------------------------------------------------*/
+
 var fs = require('fs');                 //allows operations on files (file stream)
 var xml2js = require('xml2js');         //xml parsing used to convert xml to JSON
 var convert = require('xml-js');        //parsing used to convert JSON back to xml
@@ -6,27 +9,41 @@ const shell = require('electron').shell;
 const path = require('path');
 
 
-// variables related to file name. Edit thise before running this script.
-// xml file should be named '[sitename]-basic-page.xml' ans should be saved to the 'xml-import' folder.
+
+/*---------------------------------------------------------------------------*/
+// variables related to file name. Edit this before running this script.
+// xml file should be named '[sitename]-basic-page.xml'
+// file should be saved to the 'xml-import' folder.
 // change the website variable to [sitename]
-var website = 'education';
-var fileNameLoc = 'xml-import/' + website +'-basic-page.xml';
-var modFileNameLoc = 'xml-import/' + website +'-basic-page-modified.xml';
+/*---------------------------------------------------------------------------*/
 
+/*EDIT THIS */  var website = 'education';
+/*EDIT THIS */  var websiteTitle = 'The Cato College of Education';
+///*EDIT THIS */  var stageProdSame = true;
 
-var bodyHtmlObject;                    //xml containing html is converted into JSON and stored in this variable.
-var bodyHtmlString;                    //object is converted into a string.  This variable is manipulated throughout this file by functions to scrub the html
-var bodyHtmlObjectModified;            //subset of text from within the html string
-var bodyHtmlObjectToXml;
+var stageSiteUrl = website + '.d07-stage.uncc.edu';
 
-
-// copy the file from the import folder (1) and paste it to the new location (2) using a new filename
-fs.createReadStream(fileNameLoc);
-    //.pipe(fs.createWriteStream(modFileNameLoc));
+var fileNameLoc = 'xml-import/' + website + '-basic-page.xml';
+var modFileNameLoc = 'xml-import/' + website + '-basic-page-modified.xml';
 
 
 
-// read the newly created file and store it in the 'data' variable as an object
+/*---------------------------------------------------------------------------*/
+// other variables used throughout the code
+/*---------------------------------------------------------------------------*/
+
+var bodyHtmlObject;                    //xml file content stored as JSON
+var bodyHtmlString;                    //JSON stored as string. This variable is manipulated by the functiouns throughout this script
+var bodyHtmlObjectModified;            //once functions complete, modified String stored as JSON
+var bodyHtmlObjectToXml;               //JSON back to XML. This is written to new xml file
+
+
+
+/*---------------------------------------------------------------------------*/
+// main body functionality
+/*---------------------------------------------------------------------------*/
+
+// read the xml file and store it in the 'data' variable as an object
 fs.readFile(fileNameLoc, (err, data) => {
     if (err) throw err;
     //console.log(data.toString());
@@ -40,12 +57,19 @@ fs.readFile(fileNameLoc, (err, data) => {
      parser.parseString(data, function (err, result) {
         
         console.log("\nThere are " + result.nodes.node.length + " pages being parsed.\n");
-        bodyHtmlObject = result;
-        bodyHtmlString = JSON.stringify(result);  
+        bodyHtmlObject = result;                    //store xml object notation in global variable 
+        bodyHtmlString = JSON.stringify(result);    //store xml as string for manipulation
     });
    
     // perform operations on html string
+    removeStyleAtt();
+    makeLinksRelative();
+    pointUrlsToStage();
+    removeTargetAtt();
+    setCorrectTargetAtt();
     removeSpans();
+
+    
     
     // convert from string back to json back to xml
     bodyHtmlObjectModified = JSON.parse(bodyHtmlString);
@@ -54,7 +78,8 @@ fs.readFile(fileNameLoc, (err, data) => {
     
     // for testing purposes print the contents of the altered html string
     //console.log("\n\n\n********** New XML file: **********\n");
-    //console.log(bodyHtmlObjectToXml);
+    //console.log(bodyHtmlString);
+    
     
     //create a new xml file which contains our modified html.
     fs.writeFile(modFileNameLoc, bodyHtmlObjectToXml, function(err) {
@@ -71,19 +96,92 @@ fs.readFile(fileNameLoc, (err, data) => {
 
 
 
+/*---------------------------------------------------------------------------*/
 // functions for parsing and updating html content
+/*---------------------------------------------------------------------------*/
+
+function removeStyleAtt() {
+    
+    //regex for span opening and closing tag
+    var styleAttRegEx = /style=\\"[\w:\s()\.,;\-!%']*\\"\s*/g;
+    
+    //replace span tags with nothing -- essentially deletes them
+    bodyHtmlString = bodyHtmlString.replace(styleAttRegEx, '');
+   
+    console.log('All style tags have been removed.');
+}
+
+
+function makeLinksRelative() {
+    
+    // regex http or https that point to the old site and have a trailing /
+    //regex --> /href=\\"https?:\/\/(www\.)?education.uncc.edu(\/)(?!\s)/g
+    var hrefInternalRegex = new RegExp("href=\\\\\"https?:\/\/(www\\.)?" + website + ".uncc.edu(\/)(?!\\s)","g");
+    
+    //replace with "/" to create relative link
+    bodyHtmlString = bodyHtmlString.replace(hrefInternalRegex, '$2');
+   
+    console.log('Links are now relative.');
+}
+
+
+function pointUrlsToStage() {
+    
+    var prodUrlRegEx = new RegExp(website + "\\.uncc\\.edu","g");
+    //var matches = bodyHtmlString.match(website + ".uncc.edu");
+    //console.log(prodUrlRegEx);
+    
+    bodyHtmlString = bodyHtmlString.replace(prodUrlRegEx, stageSiteUrl);
+    console.log('All urls have been pointed to stage.');
+    
+    //note: this section needs works. if the lable text for the anchor is education.uncc.edu for example it will be changed to display eduction.d07-stage.uncc.edu
+}
+
+function removeTargetAtt() {
+    
+    var targetRegex = /target=\\"_[a-zA-Z]*\\"/g;
+    //var matches = bodyHtmlString.match(targetRegex);
+    //console.log(matches);
+    
+    bodyHtmlString = bodyHtmlString.replace(targetRegex, '');
+    console.log('All target attributes have been stripped from anchor tags.');
+    
+}
+
+
+function setCorrectTargetAtt() {
+    
+    var targetRegex = /href=\\"[\w\.:\/\-%_]*\\"/g;
+    var matches = bodyHtmlString.match(targetRegex);
+    var updatedMatches;
+    
+    for (i in matches){
+        if ((!matches[i].includes(".uncc.edu")) && matches[i].includes("http")) {
+            updatedMatches = matches[i] + ' target=\\"_blank\\"';
+            //console.log(i+ " " + matches[i]);
+            //console.log(i+ " " + updatedMatches);
+            bodyHtmlString = bodyHtmlString.replace(matches[i], updatedMatches);
+        }   
+    }
+    
+    console.log('University links now open in the same window, and external links in a new tab.');
+    
+}
+
+
 function removeSpans() {
     
     //regex for span opening and closing tag
     var spanOpeningTagRegex = /<span[A-Za-z0-9\s=\\"-._:;%]*>/g;
-    var spanClosingTagRegex = /<\/span>/g 
+    var spanClosingTagRegex = /<\/span>/g; 
     
     //replace span tags with nothing -- essentially deletes them
     bodyHtmlString = bodyHtmlString.replace(spanOpeningTagRegex, '');
     bodyHtmlString = bodyHtmlString.replace(spanClosingTagRegex, '');
     
    
-    console.log('The function removeSpans() is complete');
+    console.log('All span tags have been removed.');
 }
+
 
 
